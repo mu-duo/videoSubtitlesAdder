a326707b424caeeacd247da3888771dcfb97694a
tanlinfeng <linfeng.tan@foxmail.com>
2025-11-04 18:25:42 +0800

    feat(/): Added dir process.

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ if __name__ == "__main__":
     vsa = VedioSrtAdder()
     files = sys.argv[1:]
 
+    todo = []
     if len(files) == 1 and Path(files[0]).is_dir():
-        for f in Path(files[0]).glob("*.mp4"):
-            vsa.run(f)
+        for f in Path(files[0]).rglob("*.mp4"):
+            todo.append(f)
+    for f in todo:
+        vsa.run(f)
     else:
         for f in files:
             vsa.run(f)
